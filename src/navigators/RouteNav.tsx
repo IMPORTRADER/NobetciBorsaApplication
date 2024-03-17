5815c68959cd1f58495fb23a157725961f6d0ca0
dogakocak <dogakocak>
2024-03-17 12:55:58 +0300

    onboard navigation is implemented

--- src/navigators/RouteNav.tsx
+++ src/navigators/RouteNav.tsx
@@ -3,6 +3,9 @@ import {createStackNavigator} from '@react-navigation/stack'
 import HomeScreen from '../screens/HomeScreen'
 import RootNavigator from './RootNavigator'
 import SelectStock from '../screens/SelectStock'
+import OnboardingScreen from "../screens/OnboardingScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
 
 
@@ -10,12 +13,13 @@ function HomeNavigator() {
 
   const Stack = createStackNavigator()
   return (
-    <Stack.Navigator> 
+    <Stack.Navigator initialRouteName='onboarding'>
+        <Stack.Screen name="onboarding" component={OnboardingScreen} options={{ headerShown: false }} />
         <Stack.Screen name="root" component={RootNavigator} options={{ headerShown: false }} />
         <Stack.Screen name="ismail" component={SelectStock} options={{ headerShown: false }} />
         <Stack.Screen name="home" component={HomeScreen} options={{ headerShown: false }} />
-
-
+        <Stack.Screen name="login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="register" component={RegisterScreen} options={{ headerShown: false }} />
 
     </Stack.Navigator>
   )

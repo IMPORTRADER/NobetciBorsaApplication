3122daf65cacbe62b758dc54fa5d7aed8ba52e6f
dogakocak <dogakocak>
2024-03-18 18:19:51 +0300

    auto scroll logo blur position

--- src/components/Register/styles.ts
+++ src/components/Register/styles.ts
@@ -3,7 +3,7 @@ import {StyleSheet, Dimensions} from "react-native";
 const {height,width} = Dimensions.get('window')
 const styles = StyleSheet.create({
     mainComponent:{
-        marginTop:35,
+        marginTop:25,
         justifyContent: 'center',
         alignItems: 'center'
     },
